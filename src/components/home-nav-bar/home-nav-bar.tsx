f1cc1939d02104485019ef2429ff9d07e1651420
monody157 <2737716714@qq.com>
2022-10-23 22:30:25 +0800

    修改样式

--- src/components/home-nav-bar/home-nav-bar.tsx
+++ src/components/home-nav-bar/home-nav-bar.tsx
@@ -1,10 +1,11 @@
+import React from 'react';
 import './home-nav-bar.css'
 
 function HomeNavBar() {
 
     return (
-        <div>
-            11111
+        <div className="HomeNavBar">
+            <img src='http://www.dadafastrun.com/_nuxt/img/logo.5fcbdb1.png' className="NarImage"/>
         </div>
     )
 }
