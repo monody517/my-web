c65fa78b2e9975257c6a0d6d0f85085b2f378dce
wenpei <2737716714@qq.com>
2022-10-26 16:01:24 +0800

    添加图片请求C

--- src/components/home-nav-bar/home-nav-bar.tsx
+++ src/components/home-nav-bar/home-nav-bar.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import './home-nav-bar.css'
 
 function HomeNavBar() {
+
+    const [img, setImg] = useState('')
+    
+    useEffect(() => {
+        axios.get('http://192.168.10.77:8082/api/list').then(function (res) {
+            console.log(res);
+        }).catch(function (error) {
+            // 处理错误情况
+            console.log(error);
+          })
+    },[])
 
     return (
         <div className="HomeNavBar">
